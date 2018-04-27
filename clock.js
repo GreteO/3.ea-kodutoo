9824383ee2e2989f355f530c20ae19c51f31703b
GreteO <GreteO@users.noreply.github.com>
2018-04-27 12:43:11 +0300

    Bookmarks

--- clock.js
+++ clock.js
@@ -7,6 +7,7 @@ window.onload = function () {
   document.getElementById('smaller').addEventListener("click", changeSizeSmaller)
   document.getElementById('time').addEventListener("change", changeBackground)
   document.getElementById('color').addEventListener("change", changeColor)
+  process_bookmark()
 }
 
 function init () {
@@ -16,6 +17,25 @@ function init () {
  
 }
 
+function process_bookmark(bookmarks) {
+
+  for (var i =0; i < bookmarks.length; i++) {
+ 
+       var bookmark = bookmarks[i];
+       if (bookmark.url) {
+           console.log("bookmark: "+ bookmark.title + " ~  " + bookmark.url);
+       }
+ 
+       if (bookmark.children) {
+           process_bookmark(bookmark.children);
+       }
+   }
+ }
+ 
+ chrome.browserAction.onClicked.addListener(function(tab) {
+ console.log("listing bookmarks: " );
+ chrome.bookmarks.getTree( process_bookmark );
+ });
 
 function changeBackground(){
   if (this.value == "day"){
@@ -75,3 +95,4 @@ function startClock () {
       clockContainer.innerHTML = hours+':'+minutes+':'+seconds+'<br>'+day+'.'+month+' '+year+'<br>'+weekday
   })
 }
+
